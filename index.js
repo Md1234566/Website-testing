d28737afabe73302e2779947b8e3484e90e25ee0
Bread <72562619+Md1234566@users.noreply.github.com>
2022-05-23 11:27:56 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.get('/', function(request, response) {
+  response.sendFile(__dirname + '/views/src/index.html');
+});
+app.listen(3000, () => console.log(`Website index.html is being loaded`));
